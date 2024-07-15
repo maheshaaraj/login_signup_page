6591d00420e5d09c4a9b7f5a399e0cda913f23d1
mahesharaj <99277198+maheshaaraj@users.noreply.github.com>
2024-07-16 01:35:48 +0530

    womp

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ const request = require('request');
 const path = require('path');
 
 const app = express();
-bodyParser
+
 
 // app.use(express.static(__dirname + "/styles.css"));
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public", "images")));
+
 
 // app.get('/styles.css', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'styles.css'));
 // });
 
+app.use(bodyParser.urlencoded({ extended : true }));
 
 app.get("/", (req, res) => {
     // res.send("hello from server");
     res.sendFile(__dirname + "/signup.html");
     
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    console.log("name: " + firstName + " "+ lastName +" email: " + email);
+    res.write("name: " + firstName + " "+ lastName +" email: " + email);
+    res.send();
 });
 
 
